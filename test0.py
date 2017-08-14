fb55d982ec0e8227f50b63cdfc676003c6e810d9
aiDLcnnLSTM <ai.dl.cnn.lstm@gmail.com>
2017-08-15 00:23:13 +0800

    Create test0.py

---
+++ test0.py
@@ -0,0 +1,16 @@
+import os
+import sys
+import numpy as np
+
+def mystrings():
+    web1="https://www.semanticscholar.org/"
+    pass
+
+def test():
+   
+    print('test')
+
+
+if __name__ == "__main__":
+    print('main')
+    test
